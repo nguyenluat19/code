2e43703b092e6b3a148da7bd468f310b666076be
nguyenluat19 <128703641+nguyenluat19@users.noreply.github.com>
2024-12-01 09:41:14 +0700

    Add files via upload

---
+++ login_2/demo2/src/main/java/com/example/demo/reponsitory/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.reponsitory;
+
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+}
