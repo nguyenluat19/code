e20a43a23cfdc82d16ce17dc17ac4ec8bec1b403
nguyenluat19 <128703641+nguyenluat19@users.noreply.github.com>
2024-12-09 21:29:38 +0700

    Delete phone-order directory

--- phone-order/src/main/java/com/example/phone_order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.phone_order.repository;
-
-import com.example.phone_order.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- phone-order/src/main/java/com/example/phone_order/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.phone_order.repository;
-
-import com.example.phone_order.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
